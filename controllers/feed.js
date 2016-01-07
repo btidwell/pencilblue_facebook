19f6720b6735e38e4afb4dfc775674a512e929aa
wesanderson303 <wes.anderson@careerbuilder.com>
2016-01-07 13:45:40 -0500

    pb.PluginService.getService

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,22 +1,22 @@
 module.exports = function FeedControllerModule(pb){
   var util = pb.util;
 
-  
+
   function FeedController(){}
-  
+
   util.inherits(FeedController, pb.BaseController);
-  
+
   FeedController.prototype.getPagePosts = function(cb){
     var pluginService = new pb.PluginService(this.site);
-    var OauthService = pluginService.getService("oauthService", "pencilblue_facebook", this.site);
-    var PostsService = pluginService.getService("postsService", "pencilblue_facebook", this.site);
+    var OauthService = pb.PluginService.getService("oauthService", "pencilblue_facebook", this.site);
+    var PostsService = pb.PluginService.getService("postsService", "pencilblue_facebook", this.site);
     var oauthService = new OauthService({"site":this.site});
     var postsService = new PostsService({"site":this.site});
     oauthService.getAccessToken(function(accessToken){
       postsService.getPagePosts(accessToken, cb);
     });
   };
-  
+
   FeedController.getRoutes = function(cb){
     var routes = [
       {
@@ -24,11 +24,11 @@ module.exports = function FeedControllerModule(pb){
         path: '/action/facebook/posts',
         auth_required: false,
         content_type: 'application/json',
-        handler: 'getPagePosts' 
+        handler: 'getPagePosts'
       }
     ];
     cb(null, routes);
   };
-  
+
   return FeedController;
 };
